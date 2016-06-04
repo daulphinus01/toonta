faffc28b9cccfc33523a49af12232cf3a469f8c9
daulphinus01 <daulphinus01@gmail.com>
2016-06-05 00:10:45 +0200

    Screen 1 UI almost finished

--- app/src/main/java/com/toonta/app/HomePageActivity.java
+++ app/src/main/java/com/toonta/app/HomePageActivity.java
@@ -4,12 +4,18 @@
 package com.toonta.app;
 
 import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -35,56 +41,18 @@ public class HomePageActivity extends AppCompatActivity {
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
     private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
     private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+
+    private ViewFlipper viewFlipper;
+
+    private ImageView screen1ImgTop;
+    private ImageView screen1ImgBottom;
+    private ImageView screen2ImgTop;
+    private ImageView screen2ImgBottom;
+    private ImageView screen3ImgTop;
+    private ImageView screen3ImgBottom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,23 +60,27 @@ public class HomePageActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_home_page);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        // mVisible = true;
+        // mControlsView = findViewById(R.id.fullscreen_content_controls);
+        // mContentView = findViewById(R.id.fullscreen_content);
 
         // Disabling actionbar
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null)
             actionBar.hide();
 
+        screen1ImgTop = (ImageView) findViewById(R.id.screen1_img_top);
+        screen1ImgBottom = (ImageView) findViewById(R.id.screen1_img_bottom);
+        screen2ImgTop = (ImageView) findViewById(R.id.screen2_img_top);
+        screen2ImgBottom = (ImageView) findViewById(R.id.screen2_img_bottom);
+        screen3ImgTop = (ImageView) findViewById(R.id.screen3_img_top);
+        screen3ImgBottom = (ImageView) findViewById(R.id.screen3_img_bottom);
+        loadImages();
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //toggle();
-            }
-        });
+        viewFlipper = (ViewFlipper) findViewById(R.id.view_flipper);
+
+        viewFlipper.startFlipping();
+
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
@@ -126,46 +98,36 @@ public class HomePageActivity extends AppCompatActivity {
         // delayedHide(100);
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    @Override
+    protected void onStop() {
+        super.onStop();
+        viewFlipper.stopFlipping();
+        viewFlipper = null;
     }
 
     /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
+     * Loads images from /assets
      */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    private void loadImages() {
+        try {
+            // get input stream
+            InputStream ims1 = getAssets().open("image1.png");
+            InputStream ims2 = getAssets().open("image2.png");
+            InputStream ims3 = getAssets().open("image3.png");
+            InputStream ims4 = getAssets().open("image4.png");
+            InputStream ims5 = getAssets().open("image5.png");
+            InputStream ims6 = getAssets().open("image6.png");
+
+            // load image as Drawable and setting images
+            screen1ImgTop.setImageDrawable(Drawable.createFromStream(ims1, null));
+            screen1ImgBottom.setImageDrawable(Drawable.createFromStream(ims2, null));
+            screen2ImgTop.setImageDrawable(Drawable.createFromStream(ims3, null));
+            screen2ImgBottom.setImageDrawable(Drawable.createFromStream(ims4, null));
+            screen3ImgTop.setImageDrawable(Drawable.createFromStream(ims5, null));
+            screen3ImgBottom.setImageDrawable(Drawable.createFromStream(ims6, null));
+        }
+        catch(IOException ignored) {
+
+        }
     }
 }
