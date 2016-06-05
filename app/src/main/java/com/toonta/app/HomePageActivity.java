2760711eb876c961c889bddae068b56ca608eebf
daulphinus01 <daulphinus01@gmail.com>
2016-06-05 11:35:11 +0200

    [REF] Code cleaning

--- app/src/main/java/com/toonta/app/HomePageActivity.java
+++ app/src/main/java/com/toonta/app/HomePageActivity.java
@@ -3,14 +3,10 @@
  */
 package com.toonta.app;
 
-import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -18,31 +14,11 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ * @author Marcellin RWEGO
+ * @since 1.0.0 [03/05/2016]
+ *
  */
 public class HomePageActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-
-    private View mControlsView;
 
     private ViewFlipper viewFlipper;
 
