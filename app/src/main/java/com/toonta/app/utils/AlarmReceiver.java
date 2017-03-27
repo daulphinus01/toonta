0de4ef186721572ab8200097028439d520038e67
daulphinus01@gmail.com <rwego1985mar1988>
2017-03-27 22:25:13 +0200

    WIP : Notification

---
+++ app/src/main/java/com/toonta/app/utils/AlarmReceiver.java
@@ -0,0 +1,17 @@
+package com.toonta.app.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Marcellin RWEGO on 25/03/2017.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
